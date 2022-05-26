3124d7af46b69f8df3c035e818f4dfa047bd2738
John Hahn <johnjhhahn@gmail.com>
2022-05-26 15:15:18 -0500

    Added how to read the input and class Attraction

--- Algos_Final_Project.py
+++ Algos_Final_Project.py
@@ -1,3 +1,41 @@
 # CS272 Final Project
 # Suhaas, Steven, John
 
+import sys
+import numpy as np
+
+GRID_SIZE = 400
+TIME = 1440
+
+class Attraction:
+    def __init__(self, x, y, open, close, util, duration):
+        self.x = x
+        self.y = y
+        self.open = open
+        self.close = close
+        self.util = util
+        self.duration = duration
+    
+
+def greedy():
+    return
+
+
+def read_input():
+    N = int(input())
+    attractions = []
+    for i in range(N):
+        x, y, open, close, util, duration = [int(i) for i in input().split()]
+        a1 = Attraction(x, y, open, close, util, duration)
+        attractions.append(a1)
+    return N, attractions
+
+
+def main():
+    N, attractions = read_input()
+    optimal_route = greedy(N, attractions)
+    print(optimal_route)
+
+
+if __name__ == '__main__':
+    main()

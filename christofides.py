d3714e1982f376a6948a5ab927cf7b8613584640
John Hahn <johnjhhahn@gmail.com>
2022-05-28 16:04:00 -0500

    Everything

---
+++ christofides.py
@@ -0,0 +1,228 @@
+import math
+
+def tsp(data):
+    # build a graph
+    G = build_graph(data)
+    print("Graph: ", G)
+
+    # build a minimum spanning tree
+    MSTree = minimum_spanning_tree(G)
+    print("MSTree: ", MSTree)
+
+    # find odd vertexes
+    odd_vertexes = find_odd_vertexes(MSTree)
+    print("Odd vertexes in MSTree: ", odd_vertexes)
+
+    # add minimum weight matching edges to MST
+    minimum_weight_matching(MSTree, G, odd_vertexes)
+    print("Minimum weight matching: ", MSTree)
+
+    # find an eulerian tour
+    eulerian_tour = find_eulerian_tour(MSTree, G)
+
+    print("Eulerian tour: ", eulerian_tour)
+
+    current = eulerian_tour[0]
+    path = [current]
+    visited = [False] * len(eulerian_tour)
+    visited[eulerian_tour[0]] = True
+    length = 0
+
+    for v in eulerian_tour:
+        if not visited[v]:
+            path.append(v)
+            visited[v] = True
+
+            length += G[current][v]
+            current = v
+
+    length +=G[current][eulerian_tour[0]]
+    path.append(eulerian_tour[0])
+
+    print("Result path: ", path)
+    print("Result length of the path: ", length)
+
+    return length, path
+
+def travel_time(x1, x2, y1, y2):
+    return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+def cost(start, end):
+    dist = travel_time(start[0], start[1], end[0], end[1])
+    time = end[5]
+    util = end[4]
+    return (dist**2) * (time**1.4) / (util**3)
+
+def build_graph(data):
+    graph = {}
+    for this in range(len(data)):
+        for another_point in range(len(data)):
+            if this != another_point:
+                if this not in graph:
+                    graph[this] = {}
+
+                graph[this][another_point] = cost(data[this], data[another_point])
+
+    return graph
+
+
+class UnionFind:
+    def __init__(self):
+        self.weights = {}
+        self.parents = {}
+
+    def __getitem__(self, object):
+        if object not in self.parents:
+            self.parents[object] = object
+            self.weights[object] = 1
+            return object
+
+        # find path of objects leading to the root
+        path = [object]
+        root = self.parents[object]
+        while root != path[-1]:
+            path.append(root)
+            root = self.parents[root]
+
+        # compress the path and return
+        for ancestor in path:
+            self.parents[ancestor] = root
+        return root
+
+    def __iter__(self):
+        return iter(self.parents)
+
+    def union(self, *objects):
+        roots = [self[x] for x in objects]
+        heaviest = max([(self.weights[r], r) for r in roots])[1]
+        for r in roots:
+            if r != heaviest:
+                self.weights[heaviest] += self.weights[r]
+                self.parents[r] = heaviest
+
+
+def minimum_spanning_tree(G):
+    tree = []
+    subtrees = UnionFind()
+    for W, u, v in sorted((G[u][v], u, v) for u in G for v in G[u]):
+        if subtrees[u] != subtrees[v]:
+            tree.append((u, v, W))
+            subtrees.union(u, v)
+
+    return tree
+
+
+def find_odd_vertexes(MST):
+    tmp_g = {}
+    vertexes = []
+    for edge in MST:
+        if edge[0] not in tmp_g:
+            tmp_g[edge[0]] = 0
+
+        if edge[1] not in tmp_g:
+            tmp_g[edge[1]] = 0
+
+        tmp_g[edge[0]] += 1
+        tmp_g[edge[1]] += 1
+
+    for vertex in tmp_g:
+        if tmp_g[vertex] % 2 == 1:
+            vertexes.append(vertex)
+
+    return vertexes
+
+
+def minimum_weight_matching(MST, G, odd_vert):
+    import random
+    random.shuffle(odd_vert)
+
+    while odd_vert:
+        v = odd_vert.pop()
+        length = float("inf")
+        u = 1
+        closest = 0
+        for u in odd_vert:
+            if v != u and G[v][u] < length:
+                length = G[v][u]
+                closest = u
+
+        MST.append((v, closest, length))
+        odd_vert.remove(closest)
+
+
+def find_eulerian_tour(MatchedMSTree, G):
+    # find neigbours
+    neighbours = {}
+    for edge in MatchedMSTree:
+        if edge[0] not in neighbours:
+            neighbours[edge[0]] = []
+
+        if edge[1] not in neighbours:
+            neighbours[edge[1]] = []
+
+        neighbours[edge[0]].append(edge[1])
+        neighbours[edge[1]].append(edge[0])
+
+    # print("Neighbours: ", neighbours)
+
+    # finds the hamiltonian circuit
+    start_vertex = MatchedMSTree[0][0]
+    EP = [neighbours[start_vertex][0]]
+
+    while len(MatchedMSTree) > 0:
+        for i, v in enumerate(EP):
+            if len(neighbours[v]) > 0:
+                break
+
+        while len(neighbours[v]) > 0:
+            w = neighbours[v][0]
+
+            remove_edge_from_matchedMST(MatchedMSTree, v, w)
+
+            del neighbours[v][(neighbours[v].index(w))]
+            del neighbours[w][(neighbours[w].index(v))]
+
+            i += 1
+            EP.insert(i, w)
+
+            v = w
+
+    return EP
+
+
+def remove_edge_from_matchedMST(MatchedMST, v1, v2):
+
+    for i, item in enumerate(MatchedMST):
+        if (item[0] == v2 and item[1] == v1) or (item[0] == v1 and item[1] == v2):
+            del MatchedMST[i]
+
+    return MatchedMST
+
+
+def read_input():
+    N = int(input())
+    attractions = []
+    for i in range(N):
+        x, y, open, close, util, duration = [int(i) for i in input().split()]
+        attractions.append([x, y, open, close, util, duration])
+    print(attractions)
+    return N, attractions
+
+# SAMPLE INPUT
+# 3
+# x-coord y-coord open-time close-time util duration
+# 50 100 0 1440 50 100
+# 100 100 20 50 1000 50
+#
+
+
+def main():
+    N, attractions = read_input()
+    optimal_route = tsp(attractions)
+    print(optimal_route)
+
+
+if __name__ == '__main__':
+    main()
+
+#tsp([[1, 0], [2, 0], [8, 0]])
